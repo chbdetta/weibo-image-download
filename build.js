43f3dd6476b56c2c72f1d596ec9e589300815598
chbdetta <huanbin.c@gmail.com>
2016-03-30 14:55:38 +0800

    complete

--- build.js
+++ build.js
@@ -1,91 +1,103 @@
 'use strict';
 
-// user MutationObserver to detect dom changing
-// add "download" button to each weibo feed
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       chbdetta
+// @include      http://weibo.com/*
+// @grant        none
+// ==/UserScript==
 
-// callback function when mutation is observed
+(function () {
 
-function cb(mutations) {
-  mutations.forEach(function (record) {
-    var addedFeeds = record.addedNodes;
-    console.log(addedFeeds);
-    addedFeeds.forEach(addButton);
-  });
-}
+  // user MutationObserver to detect dom changing
+  // add "download" button to each weibo feed
 
-var button = document.createElement('li');
-var link = document.createElement('a');
-link.className = 'S_txt2 chb_download_btn';
-link.innerHTML = '\n<span class="pos">\n  <span class="line S_line_1">\n    <em>download</em>\n  </span>\n</span>\n';
-button.appendChild(link);
-
-function addButton(feed) {
-  var handle = feed.querySelector('.WB_row_line.WB_row_r4');
-  if (handle) {
-    handle.appendChild(button.cloneNode(true));
+  // callback function when mutation is observed
+  function cb(mutations) {
+    mutations.forEach(function (record) {
+      var addedFeeds = record.addedNodes;
+      console.log(addedFeeds);
+      addedFeeds.forEach(addButton);
+    });
   }
-}
 
-function parent(node, filter) {
-  if (filter(node) || node === document.body) {
-    return node;
-  }
-  return parent(node.parentElement, filter);
-}
-
-function downloadAll(urls) {
+  var button = document.createElement('li');
   var link = document.createElement('a');
+  link.className = 'S_txt2 chb_download_btn';
+  link.innerHTML = '\n  <span class="pos">\n  <span class="line S_line_1">\n  <em>download</em>\n  </span>\n  </span>\n  ';
+  button.appendChild(link);
 
-  link.setAttribute('download', null);
-  link.style.display = 'none';
-
-  document.body.appendChild(link);
-
-  for (var i = 0; i < urls.length; i++) {
-    link.setAttribute('href', urls[i]);
-    link.click();
-  }
-
-  document.body.removeChild(link);
-}
-
-function getLargePicAddress(picNodes) {
-  return [].map.call(picNodes, function (node) {
-    return 'http://ww4.sinaimg.cn/large/' + node.src.split('/').reverse()[0];
-  });
-}
-
-function getPicNodes(feed) {
-  return feed.querySelectorAll('li.WB_pic img');
-}
-
-var timer = setInterval(function () {
-  if (document.querySelector('.WB_feed') === null) {
-    return;
-  }
-  clearInterval(timer);
-  var feedList = document.querySelector('.WB_feed');
-
-  feedList.addEventListener('click', function (e) {
-    if (parent(e.target, function (node) {
-      return node.classList.contains('chb_download_btn');
-    }) !== document.body) {
-      var feed = parent(e.target, function (node) {
-        return node.classList.contains('WB_cardwrap');
-      });
-      var picNodes = getPicNodes(feed);
-      var addresses = getLargePicAddress(picNodes);
-      downloadAll(addresses);
+  function addButton(feed) {
+    var handle = feed.querySelector('.WB_row_line.WB_row_r4');
+    if (handle) {
+      handle.appendChild(button.cloneNode(true));
     }
-  });
+  }
 
-  var observer = new MutationObserver(cb);
-  // observer child list change since we care about feed dynamic adding
-  observer.observe(feedList, { childList: true });
-}, 100);
+  function parent(node, filter) {
+    if (filter(node) || node === document.body) {
+      return node;
+    }
+    return parent(node.parentElement, filter);
+  }
 
-// insert css
-var head = document.querySelector('head');
-var style = document.createElement('style');
-style.innerHTML = '\n.WB_row_r4 li {\n  width: 20%; !important\n}\n';
-head.appendChild(style);
+  function downloadAll(urls) {
+    var link = document.createElement('a');
+
+    link.setAttribute('download', null);
+    link.style.display = 'none';
+
+    document.body.appendChild(link);
+
+    for (var i = 0; i < urls.length; i++) {
+      link.setAttribute('href', urls[i]);
+      link.click();
+    }
+
+    document.body.removeChild(link);
+  }
+
+  function getLargePicAddress(picNodes) {
+    return [].map.call(picNodes, function (node) {
+      return 'http://ww4.sinaimg.cn/large/' + node.src.split('/').reverse()[0];
+    });
+  }
+
+  function getPicNodes(feed) {
+    return feed.querySelectorAll('li.WB_pic img');
+  }
+
+  var timer = setInterval(function () {
+    if (document.querySelector('.WB_feed') === null) {
+      return;
+    }
+    clearInterval(timer);
+    var feedList = document.querySelector('.WB_feed');
+
+    feedList.addEventListener('click', function (e) {
+      if (parent(e.target, function (node) {
+        return node.classList.contains('chb_download_btn');
+      }) !== document.body) {
+        var feed = parent(e.target, function (node) {
+          return node.classList.contains('WB_cardwrap');
+        });
+        var picNodes = getPicNodes(feed);
+        var addresses = getLargePicAddress(picNodes);
+        downloadAll(addresses);
+      }
+    });
+
+    var observer = new MutationObserver(cb);
+    // observer child list change since we care about feed dynamic adding
+    observer.observe(feedList, { childList: true });
+  }, 100);
+
+  // insert css
+  var head = document.querySelector('head');
+  var style = document.createElement('style');
+  style.innerHTML = '\n  .WB_row_r4 li {\n    width: 20%; !important\n  }\n  ';
+  head.appendChild(style);
+})();
--- weibo.js
+++ weibo.js
@@ -1,96 +1,109 @@
-// user MutationObserver to detect dom changing
-// add "download" button to each weibo feed
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       chbdetta
+// @include      http://weibo.com/*
+// @grant        none
+// ==/UserScript==
 
-// callback function when mutation is observed
+(function() {
 
-function cb(mutations) {
-  mutations.forEach((record) => {
-    const addedFeeds = record.addedNodes
-    console.log(addedFeeds)
-    addedFeeds.forEach(addButton)
-  })
-}
+  // user MutationObserver to detect dom changing
+  // add "download" button to each weibo feed
 
-const button = document.createElement('li')
-const link = document.createElement('a')
-link.className = 'S_txt2 chb_download_btn'
-link.innerHTML = `
-<span class="pos">
+  // callback function when mutation is observed
+  function cb(mutations) {
+    mutations.forEach((record) => {
+      const addedFeeds = record.addedNodes
+      console.log(addedFeeds)
+      addedFeeds.forEach(addButton)
+    })
+  }
+
+  const button = document.createElement('li')
+  const link = document.createElement('a')
+  link.className = 'S_txt2 chb_download_btn'
+  link.innerHTML = `
+  <span class="pos">
   <span class="line S_line_1">
-    <em>download</em>
+  <em>download</em>
   </span>
-</span>
-`
-button.appendChild(link)
+  </span>
+  `
+  button.appendChild(link)
 
-function addButton(feed) {
-  const handle = feed.querySelector('.WB_row_line.WB_row_r4')
-  if (handle) {
-    handle.appendChild(button.cloneNode(true))
-  }
-}
-
-function parent(node, filter) {
-  if (filter(node) || node === document.body) {
-    return node
-  }
-  return parent(node.parentElement, filter)
-}
-
-function downloadAll(urls) {
-  var link = document.createElement('a');
-
-  link.setAttribute('download', null);
-  link.style.display = 'none';
-
-  document.body.appendChild(link);
-
-  for (var i = 0; i < urls.length; i++) {
-    link.setAttribute('href', urls[i]);
-    link.click();
-  }
-
-  document.body.removeChild(link);
-}
-
-function getLargePicAddress(picNodes) {
-  return [].map.call(picNodes, (node) => {
-    return `http://ww4.sinaimg.cn/large/${node.src.split('/').reverse()[0]}`
-  })
-}
-
-function getPicNodes(feed) {
-  return feed.querySelectorAll('li.WB_pic img')
-}
-
-let timer = setInterval(() => {
-  if (document.querySelector('.WB_feed') === null) {
-    return
-  }
-  clearInterval(timer)
-  const feedList = document.querySelector('.WB_feed')
-
-  feedList.addEventListener('click', function (e) {
-    if (parent(e.target, (node) => node.classList.contains('chb_download_btn')) !== document.body) {
-      const feed = parent(e.target, (node) => node.classList.contains('WB_cardwrap'))
-      const picNodes = getPicNodes(feed)
-      const addresses = getLargePicAddress(picNodes)
-      downloadAll(addresses)
+  function addButton(feed) {
+    const handle = feed.querySelector('.WB_row_line.WB_row_r4')
+    if (handle) {
+      handle.appendChild(button.cloneNode(true))
     }
-  })
+  }
 
-  const observer = new MutationObserver(cb)
-  // observer child list change since we care about feed dynamic adding
-  observer.observe(feedList, {childList: true})
-}, 100)
+  function parent(node, filter) {
+    if (filter(node) || node === document.body) {
+      return node
+    }
+    return parent(node.parentElement, filter)
+  }
+
+  function downloadAll(urls) {
+    var link = document.createElement('a');
+
+    link.setAttribute('download', null);
+    link.style.display = 'none';
+
+    document.body.appendChild(link);
+
+    for (var i = 0; i < urls.length; i++) {
+      link.setAttribute('href', urls[i]);
+      link.click();
+    }
+
+    document.body.removeChild(link);
+  }
+
+  function getLargePicAddress(picNodes) {
+    return [].map.call(picNodes, (node) => {
+      return `http://ww4.sinaimg.cn/large/${node.src.split('/').reverse()[0]}`
+    })
+  }
+
+  function getPicNodes(feed) {
+    return feed.querySelectorAll('li.WB_pic img')
+  }
+
+  let timer = setInterval(() => {
+    if (document.querySelector('.WB_feed') === null) {
+      return
+    }
+    clearInterval(timer)
+    const feedList = document.querySelector('.WB_feed')
+
+    feedList.addEventListener('click', function (e) {
+      if (parent(e.target, (node) => node.classList.contains('chb_download_btn')) !== document.body) {
+        const feed = parent(e.target, (node) => node.classList.contains('WB_cardwrap'))
+        const picNodes = getPicNodes(feed)
+        const addresses = getLargePicAddress(picNodes)
+        downloadAll(addresses)
+      }
+    })
+
+    const observer = new MutationObserver(cb)
+    // observer child list change since we care about feed dynamic adding
+    observer.observe(feedList, {childList: true})
+  }, 100)
 
 
-// insert css
-const head = document.querySelector('head')
-const style = document.createElement('style')
-style.innerHTML = `
-.WB_row_r4 li {
-  width: 20%; !important
-}
-`
-head.appendChild(style)
+  // insert css
+  const head = document.querySelector('head')
+  const style = document.createElement('style')
+  style.innerHTML = `
+  .WB_row_r4 li {
+    width: 20%; !important
+  }
+  `
+  head.appendChild(style)
+
+})()
